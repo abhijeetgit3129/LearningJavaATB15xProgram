61addd45b8ef5f00a0e4323dbf0afe5a46d423b3
awaghmare <awaghmare@qualys.com>
2025-12-17 19:22:15 +0530

    lab02

--- src/ex_01_java_basics/Lab_001_Hello_Word.java
+++ src/ex_01_java_basics/Lab_001_Hello_Word.java
@@ -3,5 +3,8 @@ package ex_01_java_basics;
 public class Lab_001_Hello_Word {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        char c=10;
+        System.out.println(c);
     }
 }
---
+++ src/ex_01_java_basics/Lab_002_Table.java
@@ -0,0 +1,18 @@
+package ex_01_java_basics;
+
+public class Lab_002_Table {
+    public static void main(String[]args){
+        int a=9;
+        System.out.printf("%d*1=%d%n",a,a*1);
+        System.out.printf("%d*1=%d%n",a,a*2);
+        System.out.printf("%d*1=%d%n",a,a*3);
+        System.out.printf("%d*1=%d%n",a,a*4);
+        System.out.printf("%d*1=%d%n",a,a*5);
+        System.out.printf("%d*1=%d%n",a,a*6);
+        System.out.printf("%d*1=%d%n",a,a*7);
+        System.out.printf("%d*1=%d%n",a,a*8);
+        System.out.printf("%d*1=%d%n",a,a*9);
+        System.out.printf("%d*1=%d%n",a,a*10);
+
+    }
+}
